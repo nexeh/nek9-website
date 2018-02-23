7324cfe48da774361394aa9a048a20036aaf9f94
Jeremy Corson <jcorson@dekaresearch.com>
2018-02-23 10:13:51 -0500

    updated bootstrap

--- wp-content/themes/nek9/404.php
+++ wp-content/themes/nek9/404.php
@@ -6,18 +6,11 @@
  */
 
 get_header(); ?>
-<div class="col-xs-12 col-sm-12 col-md-12">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'nek9sar' ); ?></h1>
-				</header><!-- .page-header -->
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location.', 'nek9sar' ); ?></p>
-				</div>
-			</section>
-		</main>
-	</div>
-</div>
+	<div class="parallax-section">
+	    <div class="container fill-height">
+	      <div class="row fill-height parallax-content-single">
+			<p><?php _e( 'It looks like nothing was found at this location.', 'nek9sar' ); ?></p>
+	      </div>
+	    </div>
+	  </div>	
 <?php get_footer(); ?>
