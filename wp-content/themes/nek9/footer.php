600dd21e8fb621ea77d272f150ff16f8a8bdc7ed
Amanda Perl <perl.amanda@gmail.com>
2019-02-05 10:03:48 -0500

    Update footer.php
    Adding facebook and youtube links. Reordering page display

--- wp-content/themes/nek9/footer.php
+++ wp-content/themes/nek9/footer.php
@@ -15,6 +15,9 @@
 
 				Phone: 603-526-6754<br/>
 				Pager: 877-730-2769 <br/>
+				<br>
+				<a href="https://www.facebook.com/nek9sar/"><img src="http://jeremycorson.com/nek9development/wp-content/themes/nek9/images/icon-faceboook.gif" alt="Facebook" style="width:30px;height:30px;" ></a>
+				<a href="https://www.youtube.com/channel/UC-pWNJnDTN3a3K8_4DPqm-w"><img src="http://jeremycorson.com/nek9development/wp-content/themes/nek9/images/icon-you-tube.gif" alt="YouTube" style="width:30px;height:30px;" ></a>
 
 			</div>
 			<div class="col-sm-8">
@@ -22,29 +25,30 @@
 				//for each category, show all posts
 			    $cat_args=array(
 			        'orderby' => 'name',
-			        'order' => 'ASC'
+			        'order' => 'DESC'
 			    );
 			    $categories=get_categories($cat_args);
 
 			    foreach($categories as $category) {
+			    	if ($category->name != 'Uncategorized') {
+				        $args=array(
+				            'category__in' => array($category->term_id),
+				        );
+				        $posts = get_posts($args);
 
-			        $args=array(
-			            'category__in' => array($category->term_id),
-			        );
-			        $posts = get_posts($args);
-
-			        if ($posts) {
-			        	echo '<div class="col-sm-2 pull-right">';
-			            echo '<h4>' . $category->name . '</h4> ';
-			            echo '<ul>';
-			            foreach($posts as $post) {
-			                setup_postdata($post); ?>
-			                <li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
-			                <?php
-			            } // foreach($posts
-			            echo '</ul>';
-			            echo '</div>';
-			        } // if ($posts
+				        if ($posts) {
+				        	echo '<div class="col-sm-2 col-xs-12 pull-right">';
+				            echo '<h4>' . $category->name . '</h4> ';
+				            echo '<ul>';
+				            foreach($posts as $post) {
+				                setup_postdata($post); ?>
+				                <li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+				                <?php
+				            } // foreach($posts
+				            echo '</ul>';
+				            echo '</div>';
+				        } // if ($posts
+				    }// if ($category
 			    } // foreach($categories
 			    ?>
 			</div>
